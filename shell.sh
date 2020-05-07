17d5e4f35ecac4dac270ad92b91d7d30c8a9ef9d
aviadshiber <aviadshiber@campus.technion.ac.il>
2020-05-07 08:20:28 +0300

    fixing gitbash bug

---
+++ shell.sh
@@ -0,0 +1,2 @@
+#connects to shell in docker instead of UI http://localhost:9393/dashboard
+docker exec -it dataflow-server java -jar shell.jar
---
+++ stop.sh
@@ -0,0 +1,3 @@
+SKIPPER_VERSION=2.3.2.RELEASE DATAFLOW_VERSION=2.4.2.RELEASE docker-compose -f ./docker-compose.yml \
+               -f ./docker-compose-postgres.yml \
+               -f ./docker-compose-influxdb.yml down
